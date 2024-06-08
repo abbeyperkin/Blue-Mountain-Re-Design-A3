0f75c74c57a76fd3847583ea7d7d3784f5d2863d
Abbey Perkin <aperkin@student.unimelb.edu.au>
2024-06-08 12:03:22 +1000

    Fixed home page latest additions carousel

--- results/search.js
+++ results/search.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let params = new URLSearchParams(document.location.search);
     let query = params.get("search");
-    //FIXME: breaks on null lmao
     const searchHeader = document.getElementById('searchQuery');
 
     if (query){

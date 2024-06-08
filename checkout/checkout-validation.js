c39557ad3a7ba3f7a19f4bc2d58d6075fb93f83e
Abbey Perkin <aperkin@student.unimelb.edu.au>
2024-06-08 16:36:46 +1000

    Checkout page adjustments

--- checkout/checkout-validation.js
+++ checkout/checkout-validation.js
@@ -3,3 +3,34 @@ document.getElementById('checkout-container').onsubmit = function(e) {
     window.location.href = '/confirmation';  // Redirect to the confirmation page
 };
 
+function checkIfFilled(inputElement) {
+    const userInput = inputElement.value
+    if (userInput.trim() !== '') {
+        inputElement.classList.remove('error')
+        toggleErrorMsg(inputElement, "*Required", false);
+    } else {
+        inputElement.classList.add('error')
+        toggleErrorMsg(inputElement, "*Required", true);
+    }
+}
+
+function toggleErrorMsg(inputElement, errorMsg, add) {
+    const label = document.querySelector(`label[for="${inputElement.id}"]`);
+    if (label) {
+        if (add){
+            if (!label.querySelector('.alert-text')) {
+
+                const p = document.createElement('p');
+                p.textContent = errorMsg;
+                p.className = 'alert-text';
+                label.appendChild(p);;
+            }
+        } else {
+            const existingSpan = label.querySelector('.alert');
+            const existingP = label.querySelector('.alert-text');
+            if (existingSpan) label.removeChild(existingSpan);
+            if (existingP) label.removeChild(existingP);
+        }
+
+    }
+}
